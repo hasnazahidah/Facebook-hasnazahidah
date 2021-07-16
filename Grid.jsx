72c6f4b9bea33a1ac96225d826ebe8573304529e
hasnazahidah <73343268+hasnazahidah@users.noreply.github.com>
2021-07-16 07:05:21 +0700

    Add files via upload

---
+++ Grid.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+class Grid extends Component {
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-3 col-6 test">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo, ad et mollitia exercitationem error iusto veniam, maxime recusandae, aperiam quod totam. Voluptates, facilis corrupti saepe voluptatem aspernatur vero deleniti accusamus?
+                    </div>
+                    <div className="col-md-3 col-6 test">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo, ad et mollitia exercitationem error iusto veniam, maxime recusandae, aperiam quod totam. Voluptates, facilis corrupti saepe voluptatem aspernatur vero deleniti accusamus?
+                    </div>
+                    <div className="col-md-3 col-6 test">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo, ad et mollitia exercitationem error iusto veniam, maxime recusandae, aperiam quod totam. Voluptates, facilis corrupti saepe voluptatem aspernatur vero deleniti accusamus?
+                    </div>
+                </div>
+            </div>
+
+            
+
+        )
+    }
+
+}
+
+export default Grid;
